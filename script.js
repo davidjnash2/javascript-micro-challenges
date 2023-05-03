a5ad536df09d50ec3af043a45c95c4d502f7de29
Dave Nash <davidjnash2@gmail.com>
2023-05-02 21:27:48 -0500

    wrote and tested sum equation

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log('Anyone here?');
+console.log('Good. Then let us begin.');
+
+// 100 - Addition
+// ### Example 1
+function sumTotal (num1, num2 ){ // declare function to add two numbers
+    sum = num1 + num2; // define result as result of addition equation
+    return sum; // return result
+} // end sumTotal function
+
+console.log('Result should be 5:', sumTotal(2, 3)); // log to test
+
+// ### Example 2 
+console.log('Result should be 9:', sumTotal( 2, 7 )); // log to test
+
+
+
+
+// 110 - Conditional 
