0f167600df64a140dbb3aa2086c5aa86d92b7bb2
Dave Nash <davidjnash2@gmail.com>
2023-05-02 23:13:43 -0500

    created function to slice defined portion of initial array, assign sliced values to new array, then concatenate the new array with a space between, also cleaned up functions sumTotal and bookend to be simpler and shorter

--- script.js
+++ script.js
@@ -2,9 +2,8 @@ console.log('Anyone here?');
 console.log('Good. Then let us begin.');
 
 // 100 - Addition
-function sumTotal ( num1, num2 ){ // declare function to add two numbers
-    sum = num1 + num2; // define result as result of addition equation
-    return sum; // return result
+function sumTotal( num1, num2 ){ // declare function to add two numbers
+    return sum = num1 + num2; // return result as sum of arguments
 } // end sumTotal function
 
 // ### Example 1
@@ -18,7 +17,7 @@ console.log('Result should be 9:', sumTotal( 2, 7 )); // log to test
 
 
 // 110 - Conditional 
-function whichIsBigger ( num1, num2 ){ //start function to evaluate the larger of two values
+function whichIsBigger( num1, num2 ){ //start function to evaluate the larger of two values
     if ( num1 > num2 ){ // start conditional if num1 value larger
         console.log( 'The first number was bigger!' ); // log statement reflecting larger num1 value
     } else if ( num2 > num1 ){ // continue conditional if num2 is bigger
@@ -40,10 +39,10 @@ whichIsBigger( 2, 7 ); // test
 
 
 
+
 // 120 Arrays
-function bookend(anArray){ // start function to find sum of first and last values of given array
-    sum = anArray[0] + anArray[anArray.length-1]; // set equation to sum first index value and last index value of the array
-    return sum; // return sum of equation
+function bookend( anArray ){ // start function to find sum of first and last values of given array
+    return sum = anArray[0] + anArray[anArray.length-1]; // return sum of first index value and last index value of the array
 } // end bookend function
 
 // ### Example 1
@@ -53,3 +52,24 @@ console.log('The expected result here is 6:', bookend(arrayOne)); // log to test
 // ### Example 2
 let arrayTwo = [6, 7, 8, 9, 10, 11, 12];
 console.log('The expected result here is 18:', bookend(arrayTwo)); // log to test bookend function with arrayTwo as argument
+
+
+
+
+// 130 Loops, Arrays, and Concatenation Checkpoint
+let thang = ["ASDF", "BLAH", "Oh!", "Hi", "There!"]; // declare thang array
+function findThatThang(anArray, index){ // start function for parameters of array and index
+    let newThang = anArray.slice(index); // declare new array with values from input array starting with indicated index through end
+    return newThang.join(' '); // set function result to new array, concatendated with space between values
+} // end thang function
+
+// Example 1
+console.log('That thang should say Hi There!', findThatThang(thang, 3)); // log to test
+
+// Example 2
+console.log('That thang should say Oh! Hi There!', findThatThang(thang, 2)) // log to test
+
+
+
+
+
